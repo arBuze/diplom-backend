d3f5fb84d89b3686f3f2b6188da8f3ca546d0317
Anna Sidorova <annalievna02@gmail.com>
2024-04-16 00:50:36 +0300

    images delete

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const { limiter } = require('./utils/constants');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, './uploads'));
+    cb(null, path.join(__dirname, './uploads/temp'));
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = '_' + Date.now() + '_' + Math.round(Math.random() * 1E9);
@@ -49,12 +49,57 @@ const fileTake = (req, res) => {
     });
     arr.push(name);
   }
+  console.log(fs.readdirSync(path.join(__dirname, './uploads/temp')), 'ASas');
   return res.status(201).send({ imageNames: arr });
 };
 
 app.use('/uploads', express.static('uploads'));
 
 app.post('/images', upload.array('images', 10), fileTake);
+app.delete('/images', (req, res) => {
+  const { fileName } = req.body;
+  const dir = path.join(__dirname, './uploads/temp', fileName);
+
+  try {
+    if (fs.existsSync(dir)) {
+      fs.unlink(dir, (err) => {
+        if (err) return res.status(404).send({ message: 'error' });
+        return res.status(200).send({ message: 'File has been deleted' });
+      });
+    }
+    return 0;
+  } catch (err) {
+    return res.status(404).send({ message: 'error' });
+  }
+});
+
+app.post('/repair', (req, res) => {
+  const { description, contact, fileNames } = req.body;
+  const namesArray = [];
+
+  fileNames.forEach((element) => {
+    const oldPath = path.join(__dirname, './uploads/temp', element);
+    const newPath = path.join(__dirname, './uploads', element);
+
+    fs.rename(oldPath, newPath, (err) => {
+      if (err) console.log(err);
+    });
+    namesArray.push(element);
+  });
+
+  console.log(namesArray, 'paths');
+
+  /* сделать создание модели заявки */
+  return res.status(201).send(
+    {
+      application: {
+        description,
+        contact,
+        files: namesArray,
+      },
+    },
+  );
+});
 
 app.use(errors());
 
